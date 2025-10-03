7a08f1a3488bf249c420c2220cceeaf4c1cd1c34
Leonora Goble <leonora.goble@colorado.edu>
2025-10-02 20:26:03 -0500

    fastapi hello world

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from enter-causal!")
+from typing import Union
+from fastapi import FastAPI
+import hello_dowhy
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/model/identified_estimand")
+def identified_estimand():
+    return {"identified_estimand": str(hello_dowhy.identified_estimand)}
+
+@app.get("/model/estimate")
+def estimate():
+    return {"estimate": str(hello_dowhy.estimate)}
+
+@app.get("/model/refute")
+def refute():
+    return {"refute": str(hello_dowhy.refute_results)}
